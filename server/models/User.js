4ba2fe5edd17f492596df533b8678f7e2bbfd3b9
Praptea-code <bhattarai.prapti00@gmail.com>
2026-03-16 08:04:23 +0545

    feat:user.js in models server

---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String },
+  phone: { type: String },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+})
+module.exports = mongoose.model('User', UserSchema)
