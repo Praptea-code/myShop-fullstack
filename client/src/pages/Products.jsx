e32571dcf3057c0492406491fa1d6daf7132167b
Praptea-code <bhattarai.prapti00@gmail.com>
2026-03-21 22:02:14 +0545

    fix:remove the duplication of import of react router dom

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Navbar from '../components/Navbar'
 import { useAuth, useCart } from '../App'
